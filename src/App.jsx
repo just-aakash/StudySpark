161b4ef51d9169db26482f94bc90d21d632da033
Lakshay Sharma <lakshaysharma190906@gmail.com>
2026-03-21 11:44:32 +0530

    Add course page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
 import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
+
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-
-const handleRegister = (data) => {
-  console.log("User registered:", data);
-  };
+import CoursePage from "./pages/CoursePage";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  const handleLogin = (data) => {
+    setUser(data);
+  };
+
+  const handleRegister = (data) => {
+    console.log("User registered:", data);
+    setUser(data);
+  };
+
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
+      <Route path="/login" element={<LoginPage onLogin={handleLogin} />} />
       <Route path="/register" element={<RegisterPage onRegister={handleRegister} />} />
+      
+      <Route path="/courses" element={<CoursePage user={user} onCourseSelect={(courses) => console.log(courses)} />
+} />
     </Routes>
   );
 }
